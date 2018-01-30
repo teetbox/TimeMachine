80aa46255fd9ab3ffe6ec05ae319af499a95165c
ttionn <matt.tian@icloud.com>
2018-01-30 14:20:52 +0800

    Initial Commit

---
+++ TimeMachine/TimeMachine.h
@@ -0,0 +1,19 @@
+//
+//  TimeMachine.h
+//  TimeMachine
+//
+//  Created by Matt Tian on 30/01/2018.
+//  Copyright © 2018 Bizersoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TimeMachine.
+FOUNDATION_EXPORT double TimeMachineVersionNumber;
+
+//! Project version string for TimeMachine.
+FOUNDATION_EXPORT const unsigned char TimeMachineVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TimeMachine/PublicHeader.h>
+
+
